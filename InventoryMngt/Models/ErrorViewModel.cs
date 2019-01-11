dc97948dccb2760ed3db51dcbad8fc8aaf6fc5e8
pravinkph123 <pravinkph123@gmail.com>
2019-01-12 01:17:09 +0530

    Add project files.

---
+++ InventoryMngt/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace InventoryMngt.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
